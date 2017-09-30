d5ca1ab09b059589503656f3dd8aa4d66668f91b
VeronikaAlifer <1109540@gmail.com>
2017-09-30 23:14:50 +0300

    edit file

--- src/test/java/base/BaseClass.java
+++ src/test/java/base/BaseClass.java
@@ -1,14 +1,12 @@
 package base;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
-
 import pages.LoginPage;
 
 public class BaseClass {
